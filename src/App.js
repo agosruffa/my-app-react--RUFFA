1dd8271077cb5c8e2e9c5e9a91519d4e3075d43e
agosruffa <69657713+agosruffa@users.noreply.github.com>
2021-03-24 20:23:27 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,23 @@ function App() {
   return (
     <div className="App">
       <h1>GYM A DISTANCIA</h1>
+      <div className="AppHeader">
+        <nav class="navbar navbar-expand-sm bg-light justify-content-center">
+          <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Servicios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
     </div>
   );
 }
 
-export default App;
+export default App; 
